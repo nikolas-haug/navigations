859cdb41206641d5d55db44c164d9e5859ef2231
Nikolas Haug <nikohaug21@gmail.com>
2020-04-14 15:09:03 -0500

    add secondary slide nav dir

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 function openNav() {
     document.getElementById('mySidenav').style.width = '250px';
     // push content
-    document.getElementById('main').style.marginLeft = '250px';
+    // document.getElementById('main').style.marginLeft = '250px';
     // add opacity
     document.getElementById('main').style.backgroundColor = 'rgba(0, 0, 0, 0.4)';
 }
@@ -10,7 +10,7 @@ function openNav() {
 function closeNav() {
     document.getElementById('mySidenav').style.width = '0';
     // push content
-    document.getElementById('main').style.marginLeft = '0';
+    // document.getElementById('main').style.marginLeft = '0';
     // set to default bg color
     document.getElementById('main').style.backgroundColor = 'white';
 }
